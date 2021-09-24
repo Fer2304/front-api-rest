8760e371b0cf12c21ede2165d31308ae4eb4ed2f
Fer2304 <fernando.madridfm@hotmail.com>
2021-09-24 22:06:20 +0200

    creado el component para mostrar los pedidos

--- src/app/components/chart-pedidos/chart-pedidos.component.ts
+++ src/app/components/chart-pedidos/chart-pedidos.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ClientesService} from '../../services/clientes.service';
+import { Cliente} from '../../clases/Cliente';
+
 @Component({
   selector: 'app-chart-pedidos',
   templateUrl: './chart-pedidos.component.html',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChartPedidosComponent implements OnInit {
 
-  constructor() { }
+  clientes: Cliente[];
+
+  constructor(private clientesService: ClientesService) { }
 
   ngOnInit(): void {
+    console.log('Estaos imprimiendo desde char-pedidos.component');
+    this.clientesService.getClientes().subscribe(
+      res => console.log(res)
+
+    )
   }
 
 }
